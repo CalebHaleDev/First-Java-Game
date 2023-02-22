ca34672d26c882bc3818f34ba2e0d47e0ad9379f
CalebHaleDev <107201448+CalebHaleDev@users.noreply.github.com>
2023-02-21 19:39:05 -0700

    testing timing, begin outlining

--- game.java
+++ game.java
@@ -1,47 +1,55 @@
 import java.sql.Time;
 import java.util.ArrayList;
-import java.util.Date;
+//import java.util.Date;
 import java.util.Scanner; //used for inputs? new scanner = new Scanner(System.in)  then  = scanner.nextLine()   then after use, use scanner.close();
 //print %s , variable to be substituted
-import java.time.LocalDateTime;  
-import java.time.format.DateTimeFormatter; 
-import java.util.concurrent.TimeUnit;   //TimeUnit.SECONDS.sleep(1);
-import javax.swing.Timer;     
+//import java.time.LocalDateTime;  
+//import java.time.format.DateTimeFormatter; 
+//import java.util.concurrent.TimeUnit;   //TimeUnit.SECONDS.sleep(1);
+//import javax.swing.Timer;     
 import java.awt.Image;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;  //for user inputs?
 
 public class game {
-  public static void print(String x){   //for readability
+  //for readability
+  public static void println(Object x){   
     System.out.println(x);
   }
+/*
+  public static void wait(double seconds){
+    try{
+      Thread.sleep(Double.valueOf(1000*seconds).longValue());
+      }catch(Exception e){
+        println("sleep didn't work: exception e");
+      }finally{
+      }
+  }
+*/
+  public static Object now(){
+    return java.time.Clock.systemUTC().instant();
+  }
+
+  public static void updateScreen(){
+    //idk how to do visuals
+  }
+
   public static void main(String[] args) {
-    String name = "user";
-    System.out.println("Hello "+name);
-    print("Hello "+name);   //for readability
-    
-//new Timer.Start();
-//private Timer timer;
+
+    /*main update loop
+    wait 1 second (or 1.1-.1*difficulty), move mobile block down, if landed spawn another, check for and remove valid rows.
+    run screen update
+
+    main user input loop
+    wait until key pressed, if possible: move mobile block and wait .25 or .5 seconds, or difficulty/10 seconds
+    run screen update
+    */
 
     for(int i = 0; i<10; i++){
-      System.out.println(java.time.Clock.systemUTC().instant());
-
-      //TimeUnit.SECONDS.sleep(1);
-      try{
-      Thread.sleep(3000);
-      }catch(Exception e){
-        print("didn't work: e error");
-      }finally{
-
-      }
+      println(now());
+      //wait(1);
+      Thread.sleep(1000);
+     
     }
   }
 }
-
-/*
-try{
-
-}catch(){
-
-}
-*/
