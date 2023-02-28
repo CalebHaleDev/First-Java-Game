bca1f3b33c799a9e66eeb43b1d6f921af9c8540d
CalebHaleDev <107201448+CalebHaleDev@users.noreply.github.com>
2023-02-28 15:13:29 -0700

    key press inputs and blank display

--- game.java
+++ game.java
@@ -4,30 +4,27 @@ import java.util.ArrayList;
 import java.util.Scanner; //used for inputs? new scanner = new Scanner(System.in)  then  = scanner.nextLine()   then after use, use scanner.close();
 //print %s , variable to be substituted
 //import java.time.LocalDateTime;  
-//import java.time.format.DateTimeFormatter; 
-import java.util.concurrent.TimeUnit;   //TimeUnit.SECONDS.sleep(1);
+
 //import javax.swing.Timer;     
-//import java.time.Clock
+//import java.time.Clock;
 import java.awt.Image;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;  //for user inputs?
+//import java.awt.event.ActionEvent;  //for user inputs?
+
+//for keypress events
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+//for screen display
+import javax.swing.JFrame;
+import java.awt.Dimension;
+//import javax.swing.SwingUtilities;
 
 public class game {
   //for readability
   public static void println(Object x){   
     System.out.println(x);
   }
-/*
-  //TimeSpan datatype?
-  public static void wait(double seconds){
-    try{
-      Thread.sleep(Double.valueOf(1000*seconds).longValue());
-      }catch(Exception e){
-        println("sleep didn't work: exception e");
-      }finally{
-      }
-  }
-*/
+
   public static Object now(){
     return java.time.Clock.systemUTC().instant();
   }
@@ -35,11 +32,6 @@ public class game {
   public static void updateScreen(){
     //idk how to do visuals
   }
-
-
-  public static void main(String[] args) {
-
-   
 
 /*
  * if (gravityTime < now){
@@ -50,47 +42,48 @@ public class game {
  * 
  * if (userTime < now){
  * shift if possible      user input
- *  if shift successful, reset timer    userTime = now + difficulty/10 sec
+ *  if shift successful, reset timer    userTime = now + difficulty/20 sec
  * }
  * 
  * sleep 5+ milliseconds
  */
+  public static void main(String[] args){
+//throws Exception
+   
+     JFrame myJFrame = new JFrame();
+ 
+     myJFrame.addKeyListener(new KeyAdapter() {
+       public void keyPressed(KeyEvent e) {
+         int keyCode = e.getKeyCode();
+         if (keyCode == KeyEvent.VK_UP) {
+           System.out.println("Up Arrow-Key is pressed!");
+         }
+         else if (keyCode == KeyEvent.VK_DOWN) {
+           System.out.println("Down Arrow-Key is pressed!");
+         }
+         else if (keyCode == KeyEvent.VK_LEFT) {
+           System.out.println("Left Arrow-Key is pressed!");
+         }
+         else if (keyCode == KeyEvent.VK_RIGHT) {
+         System.out.println("Right Arrow-Key is pressed!");
+         }
+       }
+     });
+ 
+     myJFrame.setVisible(true);
+     myJFrame.setSize(new Dimension(900, 800));
+     myJFrame.setMinimumSize(new Dimension(700, 700));
 
 
-
-    Thread gravityThread = new Thread(){
-      public void run(){
-        //TimeUnit.SECONDS.sleep(1);
-      //Thread.sleep(1000);
-      println("gravity");
-      }
-    };
-    gravityThread.start();
-
-    //thread subclass method of threading, from website
-    //gravityUpdate gravityThread = new gravityUpdate();
-    //gravityThread.start();
-
-    //custom running code can be put directly into the thread like this too:
-    gravityUpdate gravitythread = new gravityUpdate(){
-      public void run(){
-        System.out.println("Gravity Thread Running");
-      }
-    };
-    gravitythread.start();
-
-
-    for(int i = 0; i<10; i++){
+     for(int i = 0; i<10; i++){
       println(now());
-      //wait(1);
       try{
         Thread.sleep(1000);
         }catch(Exception e){
           println("sleep didn't work: exception e");
         }finally{
         }
-      
-     
     }
   }
 }
+
